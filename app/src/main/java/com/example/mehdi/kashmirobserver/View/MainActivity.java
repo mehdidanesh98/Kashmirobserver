d0f7ccfae27595108f73ae08320b7c47485dd0ca
Mehdi <mehdidanesh98@yahoo.com>
2017-02-15 00:36:50 +0330

    26-11-95 search and other

--- app/src/main/java/com/example/mehdi/kashmirobserver/View/MainActivity.java
+++ app/src/main/java/com/example/mehdi/kashmirobserver/View/MainActivity.java
@@ -1,26 +1,47 @@
 package com.example.mehdi.kashmirobserver.View;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mehdi.kashmirobserver.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     DrawerLayout mDrawerLayout;
     NavigationView mNavigationView;
     FragmentManager mFragmentManager;
     FragmentTransaction mFragmentTransaction;
+    private MenuItem mSearchAction;
+    private boolean isSearchOpened = false;
+    private EditText edtSeach;
+    private ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +56,11 @@ public class MainActivity extends AppCompatActivity {
     private void DefinitionControl() {
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout1);
         mNavigationView = (NavigationView) findViewById(R.id.shitstuff1);
+        logo = (ImageView) findViewById(R.id.logo);
     }
 
     private void NavigationView() {
+
         mFragmentManager = getSupportFragmentManager();
         mFragmentTransaction = mFragmentManager.beginTransaction();
         mFragmentTransaction.replace(R.id.containerView, new TabFragment()).commit();
@@ -54,14 +77,14 @@ public class MainActivity extends AppCompatActivity {
                         fragmentTransaction = mFragmentManager.beginTransaction();
                         fragmentTransaction.replace(R.id.containerView, new TabFragment()).commit();
                         break;
-                    case R.id.nav_category:
-                        Toast.makeText(MainActivity.this, "Category", Toast.LENGTH_SHORT).show();
-
-                        break;
-                    case R.id.nav_more:
-                        Toast.makeText(MainActivity.this, "More", Toast.LENGTH_SHORT).show();
-
-                        break;
+//                    case R.id.nav_category:
+//                        Toast.makeText(MainActivity.this, "Category", Toast.LENGTH_SHORT).show();
+//
+//                        break;
+//                    case R.id.nav_more:
+//                        Toast.makeText(MainActivity.this, "More", Toast.LENGTH_SHORT).show();
+//
+//                        break;
                     case R.id.nav_about:
                         Toast.makeText(MainActivity.this, "About", Toast.LENGTH_SHORT).show();
 
@@ -74,20 +97,101 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name1, R.string.app_name1);
         mDrawerLayout.setDrawerListener(mDrawerToggle);
         mDrawerToggle.syncState();
-    }
-
-    public void Search(View v) {
-        Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
 
     }
-    public void moreMenu(View v) {
-        final PopupMenu popup = new PopupMenu(MainActivity.this, v);
-        final MenuInflater inflater = popup.getMenuInflater();
-        inflater.inflate(R.menu.menu_album, popup.getMenu());
-        popup.show();
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        mSearchAction = menu.findItem(R.id.action_search);
+        return super.onPrepareOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_settings:
+
+                return true;
+            case R.id.action_search:
+                handleMenuSearch();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void handleMenuSearch() {
+        ActionBar action = getSupportActionBar(); //get the actionbar
+        if (isSearchOpened) { //test if the search is open
+
+            action.setDisplayShowCustomEnabled(false); //disable a custom view inside the actionbar
+            action.setDisplayShowTitleEnabled(false); //show the title in the action bar
+
+            //hides the keyboard
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(edtSeach.getWindowToken(), 0);
+
+            //add the search icon in the action bar
+            mSearchAction.setIcon(getResources().getDrawable(R.drawable.search));
+            logo.setVisibility(View.VISIBLE);
+
+            isSearchOpened = false;
+        } else { //open the search entry
+
+            action.setDisplayShowCustomEnabled(true); //enable it to display a
+            // custom view in the action bar.
+            action.setCustomView(R.layout.search_bar);//add the custom view
+            action.setDisplayShowTitleEnabled(false); //hide the title
+
+            edtSeach = (EditText) action.getCustomView().findViewById(R.id.edtSearch); //the text editor
+
+            //this is a listener to do a search when the user clicks on search button
+            edtSeach.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                        doSearch();
+                        return true;
+                    }
+                    return false;
+                }
+            });
+
+            edtSeach.requestFocus();
+
+            //open the keyboard focused in the edtSearch
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(edtSeach, InputMethodManager.SHOW_IMPLICIT);
+
+
+            logo.setVisibility(View.GONE);
+
+            //add the close icon
+            mSearchAction.setIcon(getResources().getDrawable(R.drawable.close));
+
+            isSearchOpened = true;
+        }
+    }
+
+    private void doSearch() {
+//
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (isSearchOpened) {
+            handleMenuSearch();
+            return;
+        }
+        super.onBackPressed();
+    }
 }
